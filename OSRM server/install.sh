e99c689d60646a1e3cbc27f0d838bf09f177cd2a
MeggyHP <williamhaiweigu@gmail.com>
2025-11-03 14:44:56 +0800

    Update installation script

--- OSRM server/install.sh
+++ OSRM server/install.sh
@@ -1,4 +1,4 @@
-curl -o city.osm.pbf http://download.geofabrik.de/asia/malaysia-singapore-brunei-250911.osm.pbf
+curl -o city.osm.pbf https://download.geofabrik.de/asia/malaysia-singapore-brunei-251102.osm.pbf
 curl -o foot.lua https://raw.githubusercontent.com/Project-OSRM/osrm-backend/master/profiles/foot.lua
 docker pull osrm/osrm-backend
 docker run -t -v "$(pwd):/data" osrm/osrm-backend osrm-extract -p /data/foot.lua /data/city.osm.pbf
